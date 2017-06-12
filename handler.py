24496411423b903d60d01c68d0ff07156846ccb9
TolikT <anatoliytihomirov@yahoo.com>
2017-06-13 02:57:45 +0300

    ready to show

--- handler.py
+++ handler.py
@@ -1,20 +1,36 @@
 from settings import SLACK_BOT_TOKEN, BOT_ID
 from slackclient import SlackClient
+import sqlite3
+import requests
 
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
 
-def handle_command(command, channel):
+def handle_command(command, channel, user):
     """
         Receives commands directed at the bot and determines if they
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
+    if user != 'incognito':
+        response = "Not sure what you mean. Use the *" + EXAMPLE_COMMAND + \
                "* command with numbers, delimited by spaces."
-    if command.startswith(EXAMPLE_COMMAND):
-        response = "Sure...write some more code then I can do that!"
+        if command.startswith(EXAMPLE_COMMAND):
+            response = "Sure...write some more code then I can do that!"
+        elif command.startswith("getorderstatus"):
+            conn = sqlite3.connect("C:\db.sqlite3")
+            c = conn.cursor()
+            c.execute("select id from register_extuser where email='{}'".format(user))
+            result = c.fetchall()[0][0]
+            print result
+            c.execute("select weight, type_of_goods from orders_saleorder where user_login_id='{}'".format(result))
+            response = ''
+            for elem in c.fetchall():
+                response += "Weight is {} and type is {}\n".format(elem[0], elem[1])
+            conn.close()
+    else:
+        response = 'no information for this user'
     slack_client.api_call("chat.postMessage", channel=channel,
                           text=response, as_user=True)
 
@@ -29,7 +45,14 @@ def parse_slack_output(slack_rtm_output):
     if output_list and len(output_list) > 0:
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
+                r = requests.post('https://slack.com/api/users.list', data={'token': SLACK_BOT_TOKEN})
+                for member in r.json()['members']:
+                    if member['id'] == output['user']:
+                        user = member['profile']['email']
+                if not user:
+                    user = 'incognito'
+
                 # return text after the @ mention, whitespace removed
                 return output['text'].split(AT_BOT)[1].strip().lower(), \
-                       output['channel']
-    return None, None
+                       output['channel'], user
+    return None, None, None
--- hornsandhoovesbot.py
+++ hornsandhoovesbot.py
@@ -4,11 +4,11 @@ from handler import parse_slack_output, handle_command, slack_client
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1  # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
-        print("StarterBot connected and running!")
+        print("HornsAndHoovesBot connected and running!")
         while True:
-            command, channel = parse_slack_output(slack_client.rtm_read())
+            command, channel, user = parse_slack_output(slack_client.rtm_read())
             if command and channel:
-                handle_command(command, channel)
+                handle_command(command, channel, user)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
